e8bef343f930a3565ff5ccd60d6c453a2a496267
Wu Clan <jianhengwu0407@gmail.com>
2024-02-06 14:46:47 +0800

    Add Gitee OAuth2

--- example/main.py
+++ example/main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 import uvicorn
 
 from fastapi import Depends, FastAPI
--- src/fastapi_oauth20/clients/gitee.py
+++ src/fastapi_oauth20/clients/gitee.py
@@ -4,15 +4,14 @@ import httpx
 
 from fastapi_oauth20.oauth20 import OAuth20Base
 
-AUTHORIZE_ENDPOINT = 'https://accounts.google.com/o/oauth2/v2/auth'
-ACCESS_TOKEN_ENDPOINT = 'https://oauth2.googleapis.com/token'
+AUTHORIZE_ENDPOINT = 'https://gitee.com/oauth/authorize'
+ACCESS_TOKEN_ENDPOINT = 'https://gitee.com/oauth/token'
 REFRESH_TOKEN_ENDPOINT = ACCESS_TOKEN_ENDPOINT
-REVOKE_TOKEN_ENDPOINT = 'https://accounts.google.com/o/oauth2/revoke'
-DEFAULT_SCOPES = ['email', 'openid', 'profile']
-PROFILE_ENDPOINT = 'https://www.googleapis.com/oauth2/v1/userinfo'
+DEFAULT_SCOPES = ['user_info']
+PROFILE_ENDPOINT = 'https://gitee.com/api/v5/user'
 
 
-class GoogleOAuth20(OAuth20Base):
+class GiteeOAuth20(OAuth20Base):
     def __init__(self, client_id: str, client_secret: str):
         super().__init__(
             client_id=client_id,
@@ -20,13 +19,13 @@ class GoogleOAuth20(OAuth20Base):
             authorize_endpoint=AUTHORIZE_ENDPOINT,
             access_token_endpoint=ACCESS_TOKEN_ENDPOINT,
             refresh_token_endpoint=REFRESH_TOKEN_ENDPOINT,
-            revoke_token_endpoint=REVOKE_TOKEN_ENDPOINT,
-            oauth_callback_route_name='google',
+            revoke_token_endpoint=None,
+            oauth_callback_route_name='gitee',
             default_scopes=DEFAULT_SCOPES,
         )
 
     async def get_userinfo(self, access_token: str) -> dict:
-        """Gets user info from Google"""
+        """Get user info from Gitee"""
         headers = {'Authorization': f'Bearer {access_token}'}
         async with httpx.AsyncClient() as client:
             response = await client.get(PROFILE_ENDPOINT, headers=headers)
